fa4455a1c00817c569ac8c3f25f2e92210bdc92e
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-17 08:20:52 +0530

    Tic Tac Toe

---
+++ src/components/Icons.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+type IconProps = PropsWithChildren<{
+    name : string;
+}>
+
+const Icons = ({name}:IconProps) => {
+    switch (name) {
+        case 'circle':
+            return <Icon name="circle-thin" size={38} color={'#F7CD2E'}/>
+        break;
+        case 'cross':
+            return <Icon name="times" size={38} color={'#38CC77'}/>
+        break;
+        default:
+            return <Icon name="pencil" size={38} color={'#000000'}/>
+        break;
+    }
+}
+
+export default Icons
+
+const styles = StyleSheet.create({})
